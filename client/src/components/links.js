29a03927e6d32bf44aee62fce08553de70306ebe
Hoby Ratefy <hoby.ratefy@protonmail.com>
2026-02-28 00:31:44 +0900

    Added client folder

---
+++ client/src/components/links.js
@@ -0,0 +1,27 @@
+export const links = [
+{
+  name: "Accueil",
+  url: "/",
+  },
+  {
+  name: "À propos",
+  url: "",
+  },
+  {
+    name: "La Bibliothèque",
+    url: "",
+  },
+  {
+  name: "Business Cases",
+  url: "",
+  },
+  {
+  name: "Ressources",
+  url: "",
+  },
+  {
+  name: "Blog",
+  url: "",
+  },
+]
+
