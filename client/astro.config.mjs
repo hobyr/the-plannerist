06e64b05bb72a505c23eb2b66557a603254a04d4
Hoby Ratefy <hoby.ratefy@protonmail.com>
2026-03-20 21:30:05 +0900

    Added sitemap integration to Astro

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -3,11 +3,14 @@ import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
 import partytown from '@astrojs/partytown';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   image: {
     remotePatterns: [{ protocol: "https", hostname: "**.strapiapp.com" }, { protocol: "http" }],
   },
   adapter: netlify(),
-  integrations: [partytown({ config: { forward: ['dataLayer.push'] } })],
+  site: "https://www.theplannerist.com",
+  integrations: [partytown({ config: { forward: ['dataLayer.push'] } }), sitemap()],
 });
