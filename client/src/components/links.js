d40bc18b62a4fa043934c082f806eaa5af38d0ef
Hoby Ratefimandimby <hoby.ratefy@protonmail.com>
2026-06-11 20:19:43 +0900

    Started working on new navbar submenu layout

--- client/src/components/links.js
+++ client/src/components/links.js
@@ -16,10 +16,14 @@ export const links = [
     submenu: [
       {
         name: "The Plannerist OS",
+        tagline: "3 mois pour remettre de l'ordre dans ton activité.",
+        description: "Ton organisation n’est pas un détail, c’est le moteur invisible de ton business. Quand les process sont flous, les priorités mal définies, tout devient compliqué. The Plannerist OS est un accompagnement designé pour remttre de l’ordre et de la fluidité dans ton organisation interne en 90 jours. ",
         url: "/the-plannerist-os-coo-a-temps-partage",
       },
       {
         name: "Le Copilotage",
+        tagline: "+6 mois pour gagner des points de marge.",
+        description: "Le Co-pilotage t’aide à améliorer l’efficacité opérationnelle de ton activité pour dégager davantage de performance : optimiser ce qui existe déjà, simplifier ce qui ralentit et concentrer les efforts là où le ROI est le plus intéressant. L’objectif : convertir une meilleure exécution en gains de marge durables.",
         url: "/copilotage-chief-operating-officer-externalise",
       },
     ]
